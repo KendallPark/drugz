ca2ac15bf1ed1e9f8409385eb48ef187f5e9aed9
Kendall Park <darkidder@gmail.com>
2016-04-24 14:31:52 -0500

    added basic routes

---
+++ frontend/app/routes/quiz.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  // model() {
+  //   var response = this.store.findAll('quiz');
+  //   console.log(response);
+  //   return response;
+  // }
+});
